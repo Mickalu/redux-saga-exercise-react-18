0d813677f3b36b274b2c2ad7cbf9f467bed5640b
Micka <lucasmichaeli95@gmail.com>
2022-12-13 10:27:27 +0100

    16 - change ref name and create method for render beer image
    - imageBeerDisplayed function created

--- src/container/Beer.jsx
+++ src/container/Beer.jsx
@@ -9,60 +9,61 @@ import BeerAttributes from '../component/Game/BeerAttributes';
 
 const Beer = ({ beer }) => {
   const dispach = useDispatch();
-  const loadingBeer = useSelector((state) => state.loadingBeer);
   const currentIndex = useSelector((state) => state.currentIndex);
 
   let img_height = "auto";
-  let beerImage;
 
-  let beerImgWrapper = useRef(null);
-  let beerImg = useRef(null);
+  let _beerImgWrapperRef = useRef(null);
+  let _beerImgRef = useRef(null);
 
   const loadingBeerComponent = (<span><i className="fa fa-circle-o-notch fa-spin fa-3x fa-fw" /></span>);
 
   useEffect(() => {
-    if (beerImg.current) {
-      beerImg.current.style.width = `${beerImgWrapper.current.offsetWidth}px`;
+    if (_beerImgRef.current) {
+      _beerImgRef.current.style.width = `${_beerImgWrapperRef.current.offsetWidth}px`;
     }
   },
   [currentIndex]);
 
   const imageLoaded = () => {
     if (img_height === 'auto'){
-      beerImg.current.style.width = `${beerImgWrapper.current.offsetWidth}px`;
-      img_height = beerImg.current.style.height;
+      _beerImgRef.current.style.width = `${_beerImgWrapperRef.current.offsetWidth}px`;
+      img_height = _beerImgRef.current.style.height;
     }
     dispach(updateLoadingBeer(false));
   };
 
-  if (beer.photo_link){
-    beerImage = (
-      <img
-        height={`${img_height}px`} style={{ height: img_height, color: 'red' }}
-        className="img-responsive"
-        ref={beerImg}
-        src={beer.photo_link}
-        onLoad={imageLoaded}
-        onError={({ currentTarget }) => {
-          dispach(updateLoadingBeer(false));
-          currentTarget.src = "";
-        }}
-        role="presentation"
-        name="beerImg"
-      />
-    );
-  }
-  else {
-    beerImage = loadingBeerComponent;
-  }
+  const imageBeerDisplayed = (beer) => {
+    if (beer.photo_link){
+      return (
+        <img
+          height={`${img_height}px`} style={{ height: img_height, color: 'red' }}
+          className="img-responsive"
+          ref={_beerImgRef}
+          src={beer.photo_link}
+          onLoad={imageLoaded}
+          onError={({ currentTarget }) => {
+            dispach(updateLoadingBeer(false));
+            currentTarget.src = "";
+          }}
+          alt="beer"
+          role="presentation"
+          name="beerImg"
+        />
+      );
+    }
+    else {
+      return loadingBeerComponent;
+    }
+  };
 
   return (
     <Container>
       <Row>
         <div className="col-sm-4" />
           <div className="col-sm-4 text-center beer-col">
-            <div ref={beerImgWrapper} className="beer-img">
-             {beerImage}
+            <div ref={_beerImgWrapperRef} className="beer-img">
+             {imageBeerDisplayed(beer)}
             </div>
             <h3 className="beer_title" name="beerTitle">{beer.title}</h3>
           </div>
