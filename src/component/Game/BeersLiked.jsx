5ca1fe906ddee101406c98685dafd4cd6b99bff9
Micka <lucasmichaeli95@gmail.com>
2022-12-15 11:16:03 +0100

    20 - delete undefined security for beerId

--- src/component/Game/BeersLiked.jsx
+++ src/component/Game/BeersLiked.jsx
@@ -7,12 +7,10 @@ const BeersLiked = ({ beersLiked, beers }) => {
   return (
     <>
       {beersLiked.data.map((beerId) => {
-        if (beerId !== "undefined"){
           const beerTitle = findBeerById(beerId, beers.data).title;
           return (
             <div data-testid="beer-title" key={`div-beer-liked-${v4()}`}> {beerTitle} </div>
           );
-        }
       })}
     </>
   );
