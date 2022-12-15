45916751ed63fabbe726ae96d0dcb409b713bce1
Micka <lucasmichaeli95@gmail.com>
2022-12-15 12:05:24 +0100

    20 - Create addLikedBeers saga and removeLikedBeers saga tests

--- src/saga/__tests__/currentIndexSaga.test.js
+++ src/saga/__tests__/currentIndexSaga.test.js
@@ -1,11 +1,11 @@
 import { call, put, select } from "redux-saga/effects";
 
-import { incrementCurrentIndexBeers } from "../currentIndexSaga";
+import { incrementCurrentIndexBeers, removeBeerToLikedBeers, addBeerToLikedBeers } from "../currentIndexSaga";
 import { currentIndexSelector } from "../../selector/currentIndex";
 import { beersSelector } from "../../selector/beers";
 import { incrementCurrentIndex, resetCurrentIndex } from "../../slice/currentIndexSlice";
-import { addLikedBeers } from "../../slice/beersLiked";
-import { initBeersState, initCurrentIndexState, initCurrentIndexLastindex } from "../../utils/__testsTools__/initValues";
+import { addLikedBeers, removeLikedBeers } from "../../slice/beersLiked";
+import { initBeersState, initCurrentIndexState, initCurrentIndexLastindex, initListBeers } from "../../utils/__testsTools__/initValues";
 
 
 it("increment current index beer Saga should select beers", () => {
@@ -42,4 +42,80 @@ it("Should increment current index if next elem exist", () => {
 
   const putIncrementCurrentIndex = incrementCurrentIndexBeersGenerator.next(initCurrentIndexState);
   expect(putIncrementCurrentIndex.value).toStrictEqual(put(incrementCurrentIndex()));
+});
+
+it("removeBeerToLikedBeers should select beers", () => {
+  const removeBeerToLikedBeersGenerator = removeBeerToLikedBeers();
+
+  const selectBeers = removeBeerToLikedBeersGenerator.next();
+
+  expect(selectBeers.value).toStrictEqual(select(beersSelector));
+});
+
+it("removeBeerToLikedBeers should select currentIndex", () => {
+  const removeBeerToLikedBeersGenerator = removeBeerToLikedBeers();
+  removeBeerToLikedBeersGenerator.next();
+
+  const selectCurrentIndex = removeBeerToLikedBeersGenerator.next(initListBeers);
+
+  expect(selectCurrentIndex.value).toStrictEqual(select(currentIndexSelector));
+});
+
+it("removeBeerToLikedBeers should put remove Liked Beers", () => {
+  const removeBeerToLikedBeersGenerator = removeBeerToLikedBeers();
+  removeBeerToLikedBeersGenerator.next();
+  removeBeerToLikedBeersGenerator.next(initListBeers);
+
+  const putRemoveLikedBeer = removeBeerToLikedBeersGenerator.next(initCurrentIndexState);
+
+  expect(putRemoveLikedBeer.value).toStrictEqual(put(removeLikedBeers({beerId: initListBeers.data[0].id})));
+});
+
+it("removeBeerToLikedBeers should call increment current index", () => {
+  const removeBeerToLikedBeersGenerator = removeBeerToLikedBeers();
+  removeBeerToLikedBeersGenerator.next();
+  removeBeerToLikedBeersGenerator.next(initListBeers);
+  removeBeerToLikedBeersGenerator.next(initCurrentIndexState);
+
+  const callIncrementCurrentIndex = removeBeerToLikedBeersGenerator.next();
+
+  expect(callIncrementCurrentIndex.value).toStrictEqual(call(incrementCurrentIndexBeers));
+});
+
+it("addBeerToLikedBeers should select beers", () => {
+  const addBeerToLikedBeersGenerator = addBeerToLikedBeers();
+
+  const selectBeers = addBeerToLikedBeersGenerator.next();
+
+  expect(selectBeers.value).toStrictEqual(select(beersSelector));
+});
+
+it("addBeerToLikedBeers should select currentIndex", () => {
+  const addBeerToLikedBeersGenerator = addBeerToLikedBeers();
+  addBeerToLikedBeersGenerator.next();
+
+  const selectCurrentIndex = addBeerToLikedBeersGenerator.next(initListBeers);
+
+  expect(selectCurrentIndex.value).toStrictEqual(select(currentIndexSelector));
+});
+
+it("addBeerToLikedBeers should put addLikedBeers", () => {
+  const addBeerToLikedBeersGenerator = addBeerToLikedBeers();
+  addBeerToLikedBeersGenerator.next();
+  addBeerToLikedBeersGenerator.next(initListBeers);
+
+  const putAddLikedBeers = addBeerToLikedBeersGenerator.next(initCurrentIndexState);
+
+  expect(putAddLikedBeers.value).toStrictEqual(put(addLikedBeers({beerId: initListBeers.data[0].id})));
+});
+
+it("addBeerToLikedBeers should call incrementCurrentIndexBeers", () => {
+  const addBeerToLikedBeersGenerator = addBeerToLikedBeers();
+  addBeerToLikedBeersGenerator.next();
+  addBeerToLikedBeersGenerator.next(initListBeers);
+  addBeerToLikedBeersGenerator.next(initCurrentIndexState);
+
+  const callIncrementCurrentIndexBeers = addBeerToLikedBeersGenerator.next();
+
+  expect(callIncrementCurrentIndexBeers.value).toStrictEqual(call(incrementCurrentIndexBeers));
 });
