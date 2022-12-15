6d06bbebc1c1faf0692577d73bb7ffa8883fea23
Micka <lucasmichaeli95@gmail.com>
2022-12-15 08:31:10 +0100

    18 - use reference into addLikedBeers reducer function

--- src/slice/beersLiked.js
+++ src/slice/beersLiked.js
@@ -4,17 +4,15 @@ const initialState = {
   data: [],
 };
 
-const addLikedBeersFunction = (state, action) => (
+const addLikedBeersFunction = (state, action) => {
   state.data = [...state.data, action.payload.beerId]
- );
+};
 
 export const beersLikedSlice = createSlice({
   name:"beersLiked",
   initialState,
   reducers: {
-    addLikedBeers: (state, action) => {
-      addLikedBeersFunction(state, action);
-    },
+    addLikedBeers: addLikedBeersFunction,
   },
 });
 
