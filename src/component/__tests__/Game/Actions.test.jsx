39cd9ce84c6654ef450b82cca0631679f0b1cad6
Micka <lucasmichaeli95@gmail.com>
2022-12-19 17:26:06 +0100

    20 - add test if button like and dislike run function when click

--- src/component/__tests__/Game/Actions.test.jsx
+++ src/component/__tests__/Game/Actions.test.jsx
@@ -1,10 +1,49 @@
 import React from "react";
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 
 import Actions from "../../Game/Actions";
-import { render, getByTestId } from "../../../utils/__testsTools__/renderMethodRTL/customRenderMethod";
+import { render, getByTestId, fireEvent } from "../../../utils/__testsTools__/renderMethodRTL/customRenderMethod";
+import { currentIndexActions } from "../../../action/currentIndexActions";
+import { initListBeers } from "../../../utils/__testsTools__/initValues";
 
-const reactRedux = { useDispatch };
+const reactRedux = { useDispatch, useSelector };
+
+jest.mock("react-redux", () => ({
+  ...jest.requireActual("react-redux"),
+  useDispatch: jest.fn(),
+  useSelector: jest.fn(),
+}));
+
+const likeBeerFunction = jest.fn();
+const dislikeBeerFunction = jest.fn();
+
+const dispatchMock = (value) => {
+  switch (value.type){
+    case currentIndexActions.ADD_BEER_LIKED_BEERS:
+      likeBeerFunction();
+      break;
+
+    case currentIndexActions.REMOVE_BEER_LIKED_BEERS:
+      dislikeBeerFunction();
+      break;
+
+    default:
+      jest.fn();
+  };
+};
+
+const useSelectorMock = jest.spyOn(reactRedux, "useSelector");
+const useDispatchMock = jest.spyOn(reactRedux, "useDispatch");
+
+useSelectorMock.mockImplementation(callback => {
+  return callback({
+    beers: initListBeers,
+    currentIndex: { currentIndex: 0 },
+    beersLiked: [],
+  });
+});
+
+useDispatchMock.mockReturnValue(jest.fn(value => dispatchMock(value)));
 
 it("Should have a button like", () => {
   render(<Actions />);
@@ -16,6 +55,20 @@ it("Should have a dislike button", () => {
   render(<Actions />);
 
   expect(getByTestId("dislike-button")).toBeInTheDocument();
+});
+
+it("Should activate liked beers if click on like button", () => {
+  render(<Actions />);
+  fireEvent.click(getByTestId("like-button"));
+
+  expect(likeBeerFunction).toHaveBeenCalledTimes(1);
+});
+
+it("Should activate disliked beers if click on dislike button", () => {
+  render(<Actions />);
+  fireEvent.click(getByTestId("dislike-button"));
+
+  expect(dislikeBeerFunction).toHaveBeenCalledTimes(1);
 });
 
 it("Should match with snapShot", () => {
