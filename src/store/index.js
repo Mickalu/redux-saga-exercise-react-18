97b65d65c171e1ad168b57368e7c48933cebe838
Micka <lucasmichaeli95@gmail.com>
2023-01-03 09:06:40 +0100

    25 - add redux-persist to the app

--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,16 @@
 import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import createSagaMiddleware from "redux-saga";
+import storage from 'redux-persist/lib/storage'
+import {
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+import { persistStore } from "redux-persist";
 
 import rootSaga from "../saga";
 import beersReducer from "../slice/beersSlice";
@@ -8,23 +19,40 @@ import currentIndexReducer from "../slice/currentIndexSlice";
 import loadingBeerReducer from "../slice/loadingBeer";
 import beersLikedReducer from "../slice/beersLiked";
 import registerUserReducer from "../slice/Authentification/registerSlice";
+import tokenAuthentificationReducer from "../slice/Authentification/tokenAuthentificationSlice";
+
+const persistConfig = {
+  key: 'root',
+  storage: storage,
+};
+
 
 const sagaMiddleWare = createSagaMiddleware();
 
-const rootReducer = combineReducers({
+const rootReducers = combineReducers({
   beers: beersReducer,
   session: sessionReducer,
   currentIndex: currentIndexReducer,
   loadingBeer: loadingBeerReducer,
   beersLiked: beersLikedReducer,
   registerUser: registerUserReducer,
+  tokenAuthentification: tokenAuthentificationReducer,
 });
 
+const persistedReducer = persistReducer(persistConfig, rootReducers)
+
 export const store = configureStore({
-  reducer: rootReducer,
+  reducer: persistedReducer,
   middleware: (getDefaultMiddleware) => {
-    return getDefaultMiddleware({ thunk: false }).prepend(sagaMiddleWare);
+    return getDefaultMiddleware({
+      thunk: false,
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }).concat(sagaMiddleWare);
   }
 });
 
 sagaMiddleWare.run(rootSaga);
+
+export const persistor = persistStore(store);
