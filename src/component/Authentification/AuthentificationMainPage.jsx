48d8dc9205f22c98cfa178821dea27829c5b10a6
Micka <lucasmichaeli95@gmail.com>
2023-01-10 09:54:35 +0100

    26 - change '' to "" in jsx because of norme rules

--- src/component/Authentification/AuthentificationMainPage.jsx
+++ src/component/Authentification/AuthentificationMainPage.jsx
@@ -6,7 +6,7 @@ import FormLogin from './FormsAutentifications/FormLogin';
 import "../../assets/css/AuthentificationMainPage.css";
 
 const AuthentificationMainPage = () => (
-  <div className='formLogin__container'>
+  <div className="formLogin__container">
     <Col lg="4">
       <FormLogin />
     </Col>
