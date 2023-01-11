36168676de9a9f0f8c8e295b9373d3218451014a
Micka <lucasmichaeli95@gmail.com>
2023-01-11 14:14:11 +0100

    24 - remove functions not used anymore because pass to interacted beer

--- src/selector/__tests__/currentBeer.test.js
+++ src/selector/__tests__/currentBeer.test.js
@@ -1,86 +1,10 @@
-import { returnFirstBeerOrNull, returnBeginningBeers, getNextCurrentBeerNotLiked } from "../currentBeer";
-import { stateBeers } from "../../utils/__testsTools__/initValues/beersSliceInitValues";
+import { getBeerById } from '../currentBeer';
+import { initBeersSliceWithBeers } from '../../utils/__testsTools__/initValues/beersSliceInitValues';
 
-it("returnFirstBeerOrNull return id of first element", () => {
-  const listBeerNotLiked = [
-    { id: "1" },
-    { id: "2" },
-    { id: "3" },
-  ];
+it("getBeerById should return beer information thank its id", () => {
+  const beers = initBeersSliceWithBeers.data;
+  const beerId = '1';
+  const returnExpected = beers[0];
 
-  expect(returnFirstBeerOrNull(listBeerNotLiked)).toBe('1');
-});
-
-it("returnFirstBeerOrNull return null if listBee is empty", () => {
-  const listBeerNotLiked = []
-
-  expect(returnFirstBeerOrNull(listBeerNotLiked)).toBe(null);
-});
-
-it("returnBeginningBeers should return first id of beer not liked", () => {
-  const listBeersLiked = ["1", "2"];
-  const listBeers = stateBeers.data;
-
-  expect(returnBeginningBeers(listBeers, listBeersLiked)).toBe("3");
-});
-
-it("returnBeginningBeers should return null if all beer liked", () => {
-  const listBeersLiked = ["1", "2", "3", "4"];
-  const listBeers = stateBeers.data;
-
-  expect(returnBeginningBeers(listBeers, listBeersLiked)).toBe(null);
-});
-
-it("getNextCurrentBeerNotLiked if id null return first no liked beer", () => {
-  const listBeers = stateBeers.data;
-  const listBeersLiked = ['1', '3'];
-  const id = null;
-  const likedOrNot = false;
-
-  expect(getNextCurrentBeerNotLiked(listBeers, listBeersLiked, id, likedOrNot)).toBe("2");
-});
-
-it("getNextCurrentBeerNotLiked no action like pass next beer not liked", () => {
-  const listBeers = stateBeers.data;
-  const listBeersLiked = ['2', '4'];
-  const id = "1";
-  const likedOrNot = false;
-
-  expect(getNextCurrentBeerNotLiked(listBeers, listBeersLiked, id, likedOrNot)).toBe("3");
-});
-
-it("getNextCurrentBeerNotLiked no action like and be at last beer return no liked beer", () => {
-  const listBeers = stateBeers.data;
-  const id = "4";
-  const listBeersLiked = ['1'];
-  const likedOrNot = false;
-
-  expect(getNextCurrentBeerNotLiked(listBeers, listBeersLiked, id, likedOrNot)).toBe("2");
-});
-
-it("getNextCurrentBeerNotLiked action like pass next beer no liked", () => {
-  const listBeers = stateBeers.data;
-  const id = "1";
-  const listBeersLiked = ['1', '2'];
-  const likedOrNot = true;
-
-  expect(getNextCurrentBeerNotLiked(listBeers, listBeersLiked, id, likedOrNot)).toBe("3");
-});
-
-it("getNextCurrentBeerNotLiked action like if last beer return beginning and beer no liked", () => {
-  const listBeers = stateBeers.data;
-  const id = "4";
-  const listBeersLiked = ['1', '2', '4'];
-  const likedOrNot = true;
-
-  expect(getNextCurrentBeerNotLiked(listBeers, listBeersLiked, id, likedOrNot)).toBe("3");
-});
-
-it("getNextCurrentBeerNotLiked action like and all beer are liked so return null", () => {
-  const listBeers = stateBeers.data;
-  const id = "4";
-  const listBeersLiked = ['1', '2', '3', '4'];
-  const likedOrNot = true;
-
-  expect(getNextCurrentBeerNotLiked(listBeers, listBeersLiked, id, likedOrNot)).toBe(null);
+  expect(getBeerById(beers, beerId)).toStrictEqual(returnExpected);
 });
--- src/selector/currentBeer.js
+++ src/selector/currentBeer.js
@@ -1,51 +1,7 @@
-import lodash from "lodash";
-
-import { getBeersNotInteracted } from "./beersNotLiked";
-
 export const currentBeerSelector = (state) => (
   state.currentBeer
 );
 
 export const getBeerById = (beers, beerId) => {
   return beers.find(beer => beer.id === beerId);
-};
-
-export const returnFirstBeerOrNull = (listBeerNotLiked) => {
-  if (listBeerNotLiked.length > 0) {
-    return listBeerNotLiked[0].id;
-  }
-  else {
-    return null;
-  }
-};
-
-export const returnBeginningBeers = (listBeers, listBeersLiked) => {
-  const listNoLikedBeer = getBeersNotInteracted(listBeers, listBeersLiked);
-  return returnFirstBeerOrNull(listNoLikedBeer);
-};
-
-export const getNextCurrentBeerNotLiked = (listBeers, listBeersLiked, id, likedOrNot) => {
-  if (lodash.isNull(id)) {
-    return returnBeginningBeers(listBeers, listBeersLiked);
-  }
-  else {
-    const indexCurrentBeer = lodash.findIndex(listBeers, ((beer) => { return beer.id === id}));
-
-    if (likedOrNot) { listBeersLiked = [...listBeersLiked, id] };
-
-    if (indexCurrentBeer + 1 < listBeers.length) {
-      const endList = listBeers.slice(indexCurrentBeer + 1);
-      const restListBeerNotLiked = getBeersNotInteracted(endList, listBeersLiked);
-
-      if (restListBeerNotLiked.length > 0) {
-        return restListBeerNotLiked[0].id;
-      }
-      else {
-        return returnBeginningBeers(listBeers, listBeersLiked);
-      }
-    }
-    else {
-      return returnBeginningBeers(listBeers, listBeersLiked);
-    }
-  }
 };
