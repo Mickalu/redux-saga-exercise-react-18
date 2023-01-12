11650b9d7d411f3bb29a424ce6d253a54845ac50
Micka <lucasmichaeli95@gmail.com>
2023-01-12 09:49:20 +0100

    24 - change skipping effect of BeersLikedContainer useEffect
    - change [token] to [] -> we don't want to refresh interacted beer when
      the authentification token change

--- src/container/BeersLikedContainer.jsx
+++ src/container/BeersLikedContainer.jsx
@@ -6,15 +6,13 @@ import BeersLiked from '../component/Game/BeersLiked';
 export const BeersLikedContainer = () => {
   const beersInterractedState = useSelector((state) => state.beersInteracted);
   const beersState = useSelector((state) => state.beers);
-  const token = useSelector((state) => state.tokenAuthentification).token;
 
   const dispatch = useDispatch();
 
   useEffect(() => {
     dispatch({ type: "GET_LIKED_BEERS" });
   },
-  [token]
-  );
+  []);
 
   return (
     <>
